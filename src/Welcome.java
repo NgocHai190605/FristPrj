21e70682885afff2317619b6753c6422d86b371f
Hoang Ngoc Hai <hoangngochai190605@gmail.com>
2024-08-13 23:00:44 +0700

    First Commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.printf("Hello Hoàng Ngọc Hải\n");
+        System.out.println("Welcome to the DHKTPM19B");
+    }
+}
